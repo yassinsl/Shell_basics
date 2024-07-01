9500a546ac3ed9de65866166fe0b45516525846d
yassinsl <slawiyassin152@gmail.com>
2024-06-30 22:18:31 -0400

    up

--- C_00/ex12/ft_print_memory.c
+++ C_00/ex12/ft_print_memory.c
@@ -1,9 +1,20 @@
+#include <unistd.h>
+
 typedef unsigned long long size_l;
 typedef unsigned char size_c;
-
-void print_str(size_c *data)
+ void print_hex(size_l nbr)
+ {
+      char *base = "0123456789abcdef";
+  
+      if(nbr >= 16)
+          print_hex(nbr /16);
+      write(1, &base[nbr % 16], 1);
+}
+void print_str(size_c *str, size_t size)
 {
-    while(str[i])
+    int i = 0;
+
+    while(i < size)
     {
         if(str[i] >= 9 && str[i] <= 13)
             write(1, ".", 1);
@@ -16,31 +27,23 @@ void print_str(size_c *data)
 void print_data(size_c *data, size_t size)
 {
     int i = 0;
-
+    write(1, ":", 1);
     while(i < size)
     {
-        if( i % 2 == 0)
-            write(1, " ", 1);
-        else if((size_t)data[i] < 16);
+     if(i % 2 == 0)
+         write(1, " ", 1);
+        if((size_t)data[i] < 16)
             write(1, "0", 1);
-        print_hex((size_l)data[i])
+        print_hex((size_l)data[i]);
         i++;
     }
     while(i < 16)
     {
         if(i % 2 == 0)
             write(1, " ", 1);
-        write(1, "  ", 1);
+        write(1, "  ", 2);
         i++;
     }
-}
-void print_hex(size_l nbr)
-{
-    char *base = "0123456789abcdef";
-
-    if(nb >= 16)
-        print_hex(nb /16);
-    write(1, &base[nb % 16], 1);
 }
 void print_add(size_l addr)
 {
@@ -49,9 +52,9 @@ void print_add(size_l addr)
 
     while(i < 16)
     {
-        if(tmp < 16)
+        if(check < 16)
             write(1, "0", 1);
-        tmp /= 16;
+        check /= 16;
         i++;
     }
     print_hex(addr);
@@ -60,7 +63,7 @@ void *ft_print_memory(void *addr, unsigned int size)
 {
     int i = 0;
     size_t handle_len;
-    size_c *arr;
+    size_c *arr = addr;
 
     while(size > i * 16)
     {
@@ -68,14 +71,14 @@ void *ft_print_memory(void *addr, unsigned int size)
             handle_len = 16;
         else
             handle_len = size % 16;
-        print_add((size_l)c);
-        print_data(c, handle_len);
-        writre(1, " ", 1);
-        print_str(c);
-        c+= 16;
+        print_add((size_l)arr);
+        print_data(arr, handle_len);
+        write(1, " ", 1);
+        print_str(arr, handle_len);
+        arr += 16;
         i++;
     }
-    return(add);
+    return(addr);
 }
 int main(void)
 {
