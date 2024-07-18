85900faa6648e210eaf91879d317b71c4b3758d1
yassinsl <slawiyassin152@gmail.com>
2024-07-17 22:07:31 -0400

    up

---
+++ header/ex01/ft_boolean.h
@@ -0,0 +1,12 @@
+#ifndef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+
+#include <unistd.h>
+#define EVEN(y) y % 2 == 0
+# define EVEN_MSG "I have a pair number of arguments."
+# define ODD_MSG "I have an impair number of arguments."
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+typedef int		t_bool;
+#endif
---
+++ header/ex02/mai.c
@@ -0,0 +1,10 @@
+#include "abs.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int y = -5;
+
+    printf("%d\n", ABS(y));
+    return(0);
+}
